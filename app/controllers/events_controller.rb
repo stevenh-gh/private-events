974331c87ffb34dbc361dad4205e9f81ef83346e
steven <steven.h@live.com>
2021-01-30 14:57:55 -0800

    add Events view
    modified #create to use build instead of getting current user id from view's hidden form
    removed current user id from strong params.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,31 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    # @event = Event.new event_params
+    @event = User.find(session[:current_user_id]).hosted_events.build event_params
+
+    if @event.save
+      redirect_to event_path @event
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find params[:id]
+  end
+
+  private
+
+  def event_params
+    # params.require(:event).permit :description, :location, :date, :hosted_id
+    params.require(:event).permit :description, :location, :date
+  end
 end
