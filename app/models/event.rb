e9cd7433be1331231b2b2dce17c07f35013bc470
steven <steven.h@live.com>
2021-01-31 01:28:24 -0800

    add EventAttending model and event_attendings table
    add many-to-many association between event and user through the join table.
    used has_many :through for the m2m assocation.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :host, foreign_key: 'hosted_id', class_name: 'User'
+
+  has_many :event_attendings, foreign_key: 'attended_event_id'
+  has_many :attendees, through: :event_attendings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   validates :password, presence: true
 
   has_many :hosted_events, foreign_key: 'hosted_id', class_name: 'Event'
+
+  has_many :event_attendings, foreign_key: 'attendee_id'
+  has_many :attended_events, through: :event_attendings 
 end
