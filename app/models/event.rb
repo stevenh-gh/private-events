468fcf67a179f8e45e3f28c4c56e951c63bcf83a
steven <steven.h@live.com>
2021-01-31 16:40:22 -0800

    add scopes for past and upcoming events. add navigation links to all pages.
    user show page separates their attended events into past and upcoming events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ApplicationRecord
 
   has_many :event_attendings, foreign_key: 'attended_event_id'
   has_many :attendees, through: :event_attendings
+
+  scope :past, -> { where('date < ?', Date.today) }
+  scope :upcoming, -> { where('date > ?', Date.today) }
 end
